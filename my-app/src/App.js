f05f129af220dd24178f6196e1486c20e75cc083
bhaskarkr <officialbhaskarkumar@gmail.com>
2018-05-19 11:49:27 +0530

    conversation working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,11 +10,14 @@ var App = createReactClass({
     return{
       "humans":{},
       "stores":{},
-      "selectedconversation":[]
+      "selectedConversation":[]
     };
   },
   loadSampleData:function(){
     this.setState(samples);
+  },
+  setSelectedConversation:function(human_index){
+        this.setState({selectedConversation:this.state.humans[human_index].conversations});
   },
   render:function(){
     return (
@@ -24,11 +27,11 @@ var App = createReactClass({
           <button onClick={this.loadSampleData}>LOAD DATA</button>
           <div className="container">
               <div className="Column">
-                  <InboxPane humans={this.state.humans}/>
+                  <InboxPane humans={this.state.humans} setSelectedConversation={this.setSelectedConversation}/>
               </div>
           </div>
           <div className="className">
-
+                  <ConversationPane conversation={this.state.selectedConversation}/>
           </div>
           <div className="className">
 
@@ -39,7 +42,7 @@ var App = createReactClass({
 });
 var InboxPane = createReactClass({
   renderInboxItem:function(human){
-    return <InboxItem key={human} index={human} details={this.props.humans[human]}/> ;
+    return <InboxItem key={human} index={human} details={this.props.humans[human]} setSelectedConversation={this.props.setSelectedConversation}/> ;
   },
   render : function () {
     return (
@@ -67,22 +70,39 @@ var InboxItem = createReactClass({
     var lastMessage = conversations.sort(this.sortByDate)[0];
     return lastMessage.who + 'said: "'+lastMessage.text+'" @'+lastMessage.time.toDateString();
   },
-
+  setSelected:function(){
+    this.props.setSelectedConversation(this.props.index);
+  },
   render:function(){
     return (
       <tr>
-        <td>{this.messageSummary(this.props.details.conversations)}</td>
+        <td><a onClick={this.setSelected}>{this.messageSummary(this.props.details.conversations)}</a></td>
         <td>{this.props.index}</td>
         <td>{this.props.details.orders.sort(this.sortByDate)[0].status}</td>
       </tr>
     );
   }
 });
+var Message = createReactClass({
+    render:function(){
+     return  <p>{this.props.who} said: "{this.props.text}"</p>
+    }
+});
+
 var ConversationPane = createReactClass({
-  render:function(){
+ 
+    renderMessage:function(val){
+        return <Message who={val.who} text={val.text} key={val.time.getTime()}/>;
+    },
+    render:function(){
     return(
-      <div>
-        </div> 
+      <div id="conversation-pane">
+        <h1>Conversation</h1>
+        <h3>Select a Conversation from inbox</h3>
+        <div id="messages">
+              {this.props.conversation.map(this.renderMessage)}
+          </div>
+      </div> 
     );
   }
 });
