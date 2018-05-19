949c6185a2a4d8d00d7864463e75f54aac76e3c7
bhaskarkr <officialbhaskarkumar@gmail.com>
2018-05-19 10:31:47 +0530

    First commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,79 +1,29 @@
-//import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-var React = require('react')
-var ReactDOM = require('react-dom')
+var React = require('react');
+var ReactDOM = require('react-dom');
+var createReactClass = require('create-react-class');
 
-class App extends React.Component {
-  render() {
+var samples = require('./sample-data');
+var App = createReactClass({
+  render:function(){
     return (
-      <div classname="column">
-         <h1>
-          asjkdhsakj
-          </h1>
-      <div id="main" classname="container">
-       
-          <div >
-          <Inside />
-          <InboxPane />
+      <div>
+          <div id="header"></div>
+          <p>MY NAME IS BHASKAR</p>
+          <div className="container">
+
           </div>
-          
+      </div>
+    );
+  }
+});
+var IndexPane = createReactClass({
+  render:function(){
+    return (
+        <div id="inbox-pane">
+        <h1></h1>
         </div>
-    </div>
     );
   }
-}
-class Inside extends React.Component{
-  render(){
-    return(
-      <div id="inb">
-      <h1>
-        This is my inbox
-      </h1>
-      </div>
-    );
-  }
-}
-class InboxPane extends React.Component{
-  render(){
-    return (
-      <div id="inbox-pane">
-        <h1>
-          Inbox
-        </h1>
-        <table>
-          <thead>
-            <tr>
-              <th>Chat received</th><br/>
-              <th>Name</th><br/>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            <InboxItem />
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
-class InboxItem extends React.Component{
-  render(){
-    return (
-      <tr>
-        <td>5PM</td><br/>
-        <td>Reddy loves Pizza</td><br/>
-        <td>confirmed</td>
-      </tr>
-    );
-  }
-}
-//ReactDOM.render(<App />,document.getElementById("main"))
+});
 export default App;
-/*<header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>*/
